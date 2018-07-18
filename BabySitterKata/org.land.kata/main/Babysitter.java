de24e728d58bbf0182aa10f11cacc49db2807898
Keth4 <Keth4@DESKTOP-SCD09BT>
2018-07-18 19:16:39 -0400

    refactored herd pay functionality out into separate method

--- BabySitterKata/org.land.kata/main/Babysitter.java
+++ BabySitterKata/org.land.kata/main/Babysitter.java
@@ -6,14 +6,19 @@ public class Babysitter {
 	public static int calculateBabysitterPay(int arrival, int departure, int bedtime) {
 		int totalPay = 0;	
 		for(int i = arrival; i < departure; i++ ) {
-			if (i < bedtime) {
-				totalPay += 10;
-			}
+			totalPay = calculatesHerdPay(bedtime, totalPay, i);
 			if(i >= bedtime) {
 				totalPay += 6;
 			}
 			}
 			return totalPay;
+	}
+
+	private static int calculatesHerdPay(int bedtime, int totalPay, int i) {
+		if (i < bedtime) {
+			totalPay += 10;
+		}
+		return totalPay;
 	}
 
 
